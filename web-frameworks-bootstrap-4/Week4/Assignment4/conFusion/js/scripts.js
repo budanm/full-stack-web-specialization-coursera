75074f8eeb29652af8bb28dc74b88aced25cab85
i312345 <soumya.mukhopadhyay@sap.com>
2017-08-27 14:37:51 +0530

    np scripts

---
+++ web-frameworks-bootstrap-4/Week4/Assignment4/conFusion/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $("#carousel-button").click(function () {
+        if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass('fa-pause');
+            $("#carousel-button").children("span").addClass('fa-play');
+        }
+        else if ($("#carousel-button").children("span").hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass('fa-play');
+            $("#carousel-button").children("span").addClass('fa-pause');
+        }
+    });
+});
