3f683e23e9cc64e4ff48a593e4bee348c8e30d4b
i312345 <soumya.mukhopadhyay@sap.com>
2017-09-02 11:20:03 +0530

    configuring angular

--- Frontend Frameworks - Angular/Week1/Assignment1/confusion/src/app/app.module.ts
+++ Frontend Frameworks - Angular/Week1/Assignment1/confusion/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
