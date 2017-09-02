f372b62d868f8c9ef3b295094601f11ccc78532f
i312345 <soumya.mukhopadhyay@sap.com>
2017-09-02 11:44:59 +0530

    Components Part 1

--- Frontend Frameworks - Angular/Week1/Assignment1/confusion/src/app/app.module.ts
+++ Frontend Frameworks - Angular/Week1/Assignment1/confusion/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
