359befebbedf60ef451ca624b87031ebddff8d08
i312345 <soumya.mukhopadhyay@sap.com>
2017-09-04 10:09:31 +0530

    adding router module

--- Frontend Frameworks - Angular/Week2/Assignment2/confusion/src/app/app.module.ts
+++ Frontend Frameworks - Angular/Week2/Assignment2/confusion/src/app/app.module.ts
@@ -14,20 +14,28 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
