40039ee59fad770db4b9372702fe7a89e8818481
i312345 <soumya.mukhopadhyay@sap.com>
2017-09-04 17:42:33 +0530

    params router

--- Frontend Frameworks - Angular/Week2/Assignment2/confusion/src/app/dishdetail/dishdetail.component.ts
+++ Frontend Frameworks - Angular/Week2/Assignment2/confusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
 
+import { DishService } from '../services/dish.service';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -8,11 +12,19 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
 
-  constructor() { }
+  dish: Dish;
+
+  constructor(private dishservice: DishService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
   }
 
-  @Input()
-  dish: Dish;
+  goBack(): void {
+    this.location.back();
+  }
+
 }
--- Frontend Frameworks - Angular/Week2/Assignment2/confusion/src/app/menu/menu.component.ts
+++ Frontend Frameworks - Angular/Week2/Assignment2/confusion/src/app/menu/menu.component.ts
@@ -21,9 +21,6 @@ export class MenuComponent implements OnInit {
   }
 
 
-
-  
-
   onSelect(dish: Dish){
     this.selectedDish = dish;
   }  
