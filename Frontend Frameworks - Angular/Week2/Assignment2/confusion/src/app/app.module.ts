9b9e764b0a27cb7a884ded20857ebcc31cad6cbf
i312345 <soumya.mukhopadhyay@sap.com>
2017-09-04 10:38:09 +0530

    SPA Part 1

--- Frontend Frameworks - Angular/Week2/Assignment2/confusion/src/app/app.module.ts
+++ Frontend Frameworks - Angular/Week2/Assignment2/confusion/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
@@ -37,7 +38,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
