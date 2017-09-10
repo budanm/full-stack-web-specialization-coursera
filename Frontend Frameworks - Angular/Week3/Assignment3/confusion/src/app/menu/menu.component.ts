67ecbc6bce6c5a10fd656c6b2faa5b7d0a3c1c80
i312345 <soumya.mukhopadhyay@sap.com>
2017-09-10 13:54:53 +0530

    promise part 1

--- Frontend Frameworks - Angular/Week3/Assignment3/confusion/src/app/menu/menu.component.ts
+++ Frontend Frameworks - Angular/Week3/Assignment3/confusion/src/app/menu/menu.component.ts
@@ -17,7 +17,7 @@ export class MenuComponent implements OnInit {
   constructor(private dishService : DishService) { }
 
   ngOnInit() {
-    this.dishes = this.dishService.getDishes();
+    this.dishService.getDishes().then( dishes => this.dishes = dishes);
   }
 
 
