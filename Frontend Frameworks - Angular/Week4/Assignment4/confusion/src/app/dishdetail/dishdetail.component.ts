4030397873cedd9e1c88839f149f7383963bac03
i312345 <soumya.mukhopadhyay@sap.com>
2017-09-17 13:46:13 +0530

    animations part 1

--- Frontend Frameworks - Angular/Week4/Assignment4/confusion/src/app/dishdetail/dishdetail.component.ts
+++ Frontend Frameworks - Angular/Week4/Assignment4/confusion/src/app/dishdetail/dishdetail.component.ts
@@ -8,12 +8,29 @@ import { Location } from '@angular/common';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -24,6 +41,9 @@ export class DishdetailComponent implements OnInit {
   dishfeedbackform: FormGroup;
   dishComment: Comment;
   dishcopy = null;
+  visibility = 'shown';
+  errMess = null;
+
 
   formErrors = {
     'author': '',
@@ -53,8 +73,9 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-      .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); });
+      .switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); })
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
+      errmess => { this.dish = null; this.errMess = <any>errmess; });
 
   }
 
