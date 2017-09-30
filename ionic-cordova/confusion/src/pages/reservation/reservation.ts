a5bfdcca3916f52ffa116515462fd01b1a73dcf1
i312345 <soumya.mukhopadhyay@sap.com>
2017-09-30 08:44:46 +0530

    modal part1

---
+++ ionic-cordova/confusion/src/pages/reservation/reservation.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+
+/**
+ * Generated class for the ReservationPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-reservation',
+  templateUrl: 'reservation.html',
+})
+export class ReservationPage {
+
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public viewCtrl: ViewController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ReservationPage');
+  }
+
+  dismiss(){
+    this.viewCtrl.dismiss();
+  }
+
+}
