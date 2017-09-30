9625c99daa0886be9e6b38715508d227b3fdd93b
i312345 <soumya.mukhopadhyay@sap.com>
2017-09-30 08:52:03 +0530

    forms

--- ionic-cordova/confusion/src/pages/reservation/reservation.ts
+++ ionic-cordova/confusion/src/pages/reservation/reservation.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 /**
  * Generated class for the ReservationPage page.
  *
@@ -15,9 +15,18 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class ReservationPage {
 
+  reservation: FormGroup;
+
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
-    public viewCtrl: ViewController) {
+    public viewCtrl: ViewController,
+    private formbuilder: FormBuilder) {
+
+      this.reservation = this.formbuilder.group({
+        guests: 3,
+        smoking: false,
+        dateTime: ['', Validators.required]
+      })
   }
 
   ionViewDidLoad() {
@@ -25,6 +34,11 @@ export class ReservationPage {
   }
 
   dismiss(){
+    this.viewCtrl.dismiss();
+  }
+
+  onSubmit(){
+    console.log(this.reservation.value);
     this.viewCtrl.dismiss();
   }
 
