809156c137804210df6e79282d3049d2240c89c1
i312345 <soumya.mukhopadhyay@sap.com>
2017-10-19 13:27:57 +0530

    mongoose part2

--- Node And MongoDB/node-mongoose/index.js
+++ Node And MongoDB/node-mongoose/index.js
@@ -14,19 +14,32 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    });
-
-    newDish.save()
+    })
         .then((dish) => {
             console.log(dish);
 
-            return Dishes.find({}).exec();
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: {
+                    description: 'Updated test'
+                }
+            }).exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+
+            return dish.save();
+        })
+        .then((dish) => {
+            console.log(dish);
 
             return db.collection('dishes').drop();
         })
--- Node And MongoDB/node-mongoose/models/dishes.js
+++ Node And MongoDB/node-mongoose/models/dishes.js
@@ -1,6 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+        timestamps: true
+    });
+
+
 const dishSchema = new Schema({
     name: {
         type: String,
@@ -11,7 +31,9 @@ const dishSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
+
 }, {
         timestamps: true
     });
